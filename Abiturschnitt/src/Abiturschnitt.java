6172773f0460865ab4423f809af4b8bfe9cd0d69
Alexandra.von.Trotha <Alexandra.von.Trotha@SK-448A5BCB8A90.2631.skool.net>
2018-07-06 08:36:32 +0200

    Textdatei importieren klappt nicht, Rest von Programm schon

--- Abiturschnitt/src/Abiturschnitt.java
+++ Abiturschnitt/src/Abiturschnitt.java
@@ -14,31 +14,29 @@ public class Abiturschnitt {
 	static int punkteB1;
 	static int punkteB2;
 	
-	int [] notenBlockEins;
-	int [] notenBlockZwei;
-
+	static int[]notenBlockEins={15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, };
+	static int [] notenBlockZwei={ 15, 15, 15, 15, 15};
 	public static void main(String[] args) {
 		
-		int[]notenBlockEins={15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, };
+		//System.out.println(blockEins(notenBlockEins));
+		//System.out.println(blockZwei(notenBlockZwei));
 		
 		
-		System.out.println(blockEins(notenBlockEins));
-		}
-	
-
-	public static int blockEins(int [] notenBlockEins){
+		System.out.println(gesamt());
+	}
+	public static int blockEins (int [] notenBlockEins){
 		int sum=0;
 
 		int size=notenBlockEins.length;
 		
 		
 		
-		for (int i=0; i<=size; i++){
+		for (int i=0; i<size; i++){
 			
-			 sum = sum+notenBlockEins[i];
+			 sum = sum + notenBlockEins[i];
 		}
 		
-		if (size<=39){
+		if (size==40){
 			punkteB1=sum;
 		}
 		else{
@@ -47,26 +45,25 @@ public class Abiturschnitt {
 		return punkteB1;
 	}
 	
-	public int blockZwei( int [] notenBlockZwei){
+	public static int blockZwei( int [] notenBlockZwei){
 		int sum=0;
 
-		int size=notenBlockZwei.length;
+		int size= notenBlockZwei.length;
 		
-		for (int i=0; i<=size; i++){
+		for (int i=0; i<size; i++){
 			
-			sum = sum+ notenBlockZwei[i]*4;
+			sum = sum + notenBlockZwei[i]*4;
 		}
 		
-		punkteB2=sum;
+		punkteB2= sum;
 		
 		return punkteB2;
 		
 	}
 	
-	/*static void importFile() {
+	static void importFile() {
 	    
 	     //Liest die Datei ein und speichert sie in den String[][] array
-	     String [] array;
 		
 	    try{
 	    	
@@ -74,7 +71,7 @@ public class Abiturschnitt {
 	        String line;
 	        
 	        for(int i = 0; (line = reader.readLine()) != null; i++) {
-	            array[i] = line.split(" ");
+	            notenBlockEins[i] = line.split(" ");
 	        }
 
 	        reader.close();
@@ -87,27 +84,41 @@ public class Abiturschnitt {
 	        System.out.println ("Lesefehler");
 	    } // end catch
 	    catch (IndexOutOfBoundsException ex) {
-	        System.out.println("ups, der String[][] array ist zu klein für diese Textfilegroesse");
-	        System.out.println("maximal sind "+array.length+" Zeilen moeglich, sonst muss die Groesse von array in Zeile 9 erhoeht werden");
+	        System.out.println("ups, der int [] notenBlockEins ist zu klein für diese Textfilegroesse");
+	        System.out.println("maximal sind "+notenBlockEins.length+" Zeilen moeglich, sonst muss die Groesse von array in Zeile 9 erhoeht werden");
 	        System.exit(1);
 	    } // end try
 
-	    
+	    int foo= notenBlockEins[0];
+	    System.out.println(foo);
 	    
 	} // end importFile*/
 
 	
-	public double gesamt(){
+	public static double gesamt(){
 		
 		int gesamtpunkte= blockZwei(notenBlockZwei) + blockEins(notenBlockEins);
+		System.out.println(gesamtpunkte);
 		
 		double punkteschnitt= gesamtpunkte/60.0;
+		//System.out.println(punkteschnitt);
 		
 		double abischnitt= (17-punkteschnitt)/3;
+		//System.out.println(abischnitt);
+		
+		if (abischnitt<1.0){
+			abischnitt=1.0;
+		}
 		
 		return abischnitt;
 		
 	} 
+	
+	/*public void toString(String array[]){
+		for (int i=0; i<array.length; i++){
+			for 
+		}
+	}*/
 	
 
 }
